30725dbf0337435e70977029e4b71396fd46faad
davramuk <davramuk@gmail.com>
2023-02-16 10:40:35 +0200

    Task_2 (option 6)

---
+++ AT/src/main/java/itstep/task_2/StringConcatenation.java
@@ -0,0 +1,10 @@
+package itstep.task_2;
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String str1 = "ITSTEP";
+        String str2 = "University";
+        String result = str1 + " " + str2;
+        System.out.println(result);
+    }
+}
